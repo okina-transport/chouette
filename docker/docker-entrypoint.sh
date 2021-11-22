bec0d80e82cf4e2aafb3ec5fb1a8f6d16adcd066
mhicauber <mhicauber@okina.fr>
2021-11-22 10:37:46 +0100

    Image de base v1.2 : 10h pour le transaction timeout

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -16,7 +16,7 @@ function waitWildfly {
 function initWildfly {
 	waitWildfly
 	/opt/jboss/wildfly/bin/jboss-cli.sh -c --user=admin --password=password --file=/tmp/wildfly_db.cli
-	/opt/jboss/wildfly/bin/jboss-cli.sh -c --user=admin --password=password --command='/subsystem=transactions:write-attribute(name=default-timeout,value=3600)'
+	/opt/jboss/wildfly/bin/jboss-cli.sh -c --user=admin --password=password --command='/subsystem=transactions:write-attribute(name=default-timeout,value=36000)'
 	/opt/jboss/wildfly/bin/jboss-cli.sh -c --user=admin --password=password --command='/subsystem=undertow/server=default-server/http-listener=default/:write-attribute(name=max-post-size,value=100485760)'
 	/opt/jboss/wildfly/bin/jboss-cli.sh -c --user=admin --password=password --command="/:reload"
 	/opt/jboss/wildfly/bin/jboss-cli.sh -c --user=admin --password=password --command="deploy /tmp/chouette.ear"
