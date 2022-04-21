384171c32a7cc8c8e43473328672564ed76eccf4
vincent <vincent.paturet@entur.org>
2022-04-21 16:01:48 +0200

    Code cleanup

--- mobi.chouette.exchange.netexprofile/src/main/java/mobi/chouette/exchange/netexprofile/exporter/producer/ServiceJourneyInterchangeProducer.java
+++ mobi.chouette.exchange.netexprofile/src/main/java/mobi/chouette/exchange/netexprofile/exporter/producer/ServiceJourneyInterchangeProducer.java
@@ -1,7 +1,6 @@
 package mobi.chouette.exchange.netexprofile.exporter.producer;
 
 import mobi.chouette.common.Context;
-import mobi.chouette.common.TimeUtil;
 import mobi.chouette.exchange.netexprofile.ConversionUtil;
 import mobi.chouette.model.Interchange;
 import mobi.chouette.model.ScheduledStopPoint;
