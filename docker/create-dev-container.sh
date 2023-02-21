d30cdbc47e68ae8db4b9ebd00d8cf9ba39782054
mhicauber <mhicauber@okina.fr>
2023-02-21 19:07:20 +0100

    Base image v1.4 : All IEV DB parameters might be different from chouette ones

--- docker/create-dev-container.sh
+++ docker/create-dev-container.sh
@@ -11,6 +11,11 @@ docker run \
 --env CHOUETTE_DB_USER=chouette \
 --env CHOUETTE_DB_PASSWORD=chouette \
 --env CHOUETTE_DB_NAME=chouette \
+--env CHOUETTE_IEV_DB_HOST=192.168.24.59 \
+--env CHOUETTE_IEV_DB_PORT=5449 \
+--env CHOUETTE_IEV_DB_USER=chouette \
+--env CHOUETTE_IEV_DB_PASSWORD=chouette \
+--env CHOUETTE_IEV_DB_NAME=iev \
 --env JAVA_OPTS="-Xms1024m -Xmx2048m -XX:MetaspaceSize=256M -XX:MaxMetaspaceSize=512m -Djava.net.preferIPv4Stack=true -Djboss.modules.system.pkgs=org.jboss.byteman -Djava.awt.headless=true" \
 registry.okina.fr/mobiiti/chouette-base:1.0 \
 /opt/jboss/wildfly/bin/standalone.sh -b 0.0.0.0 -bmanagement 0.0.0.0 --debug
