d0dd29dc69632b408a5752c8c6f6ac7cc1a91e41
gcarrere <gcarrere@okina.fr>
2023-05-03 14:30:11 +0200

    16817 - update stop accessibility

--- mobi.chouette.common/src/main/java/mobi/chouette/common/Constant.java
+++ mobi.chouette.common/src/main/java/mobi/chouette/common/Constant.java
@@ -68,6 +68,7 @@ public interface Constant {
 	public static final String KEEP_STOP_GEOLOCALISATION = "KeepStopGeolocalisation";
 	public static final String KEEP_STOP_NAMES = "KeepStopNames";
 	public static final String CLOSE_OLD_CALENDARS = "CloseOldCalendars";
+	public static final String UPDATE_STOP_ACCESSIBILITY = "updateStopAccessibility";
 
 	public static final char SEP = '|';
 	public static final String NULL = "\\N";
--- mobi.chouette.exchange.netexprofile/src/main/java/mobi/chouette/exchange/netexprofile/importer/NetexprofileImporterCommand.java
+++ mobi.chouette.exchange.netexprofile/src/main/java/mobi/chouette/exchange/netexprofile/importer/NetexprofileImporterCommand.java
@@ -52,6 +52,7 @@ public class NetexprofileImporterCommand extends AbstractImporterCommand impleme
 		NetexprofileImportParameters parameters = (NetexprofileImportParameters) configuration;
 		context.put(KEEP_STOP_GEOLOCALISATION, Boolean.valueOf(parameters.isKeepStopGeolocalisation()));
 		context.put(KEEP_STOP_NAMES, Boolean.valueOf(parameters.isKeepStopNames()));
+		context.put(UPDATE_STOP_ACCESSIBILITY, Boolean.FALSE);
 
 		ProcessingCommands commands = ProcessingCommandsFactory.create(NetexImporterProcessingCommands.class.getName());
 		result = process(context, commands, progression, true, Mode.line);
