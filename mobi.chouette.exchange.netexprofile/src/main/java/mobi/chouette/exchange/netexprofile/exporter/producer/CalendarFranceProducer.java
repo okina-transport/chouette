4ec6137cabb6107592dcde6e8a8a99bbe8278ec6
gcarrere <gcarrere@okina.fr>
2023-08-08 11:55:06 +0200

    Fix export Netex

--- mobi.chouette.exchange.netexprofile/src/main/java/mobi/chouette/exchange/netexprofile/exporter/producer/CalendarFranceProducer.java
+++ mobi.chouette.exchange.netexprofile/src/main/java/mobi/chouette/exchange/netexprofile/exporter/producer/CalendarFranceProducer.java
@@ -57,14 +57,12 @@ public class CalendarFranceProducer extends NetexProducer {
                     if (p.getStartDate().isBefore(p.getEndDate())) {
                         OperatingPeriodRefStructure operatingPeriodRef = netexFactory.createOperatingPeriodRefStructure();
                         // Create Operating period
-                        String operatingPeriodId = netexDaytypeId.replace("DayType", "OperatingPeriod");
+                        String operatingPeriodId = netexDaytypeId.replace("DayType", "OperatingPeriod").replace(":LOC","") + "-" + i + ":LOC";
                         OperatingPeriod operatingPeriod = new OperatingPeriod().withVersion(dayType.getVersion())
                                 .withId(operatingPeriodId)
                                 .withFromDate(TimeUtil.toLocalDateFromJoda(p.getStartDate()).atStartOfDay()).withToDate(TimeUtil.toLocalDateFromJoda(p.getEndDate()).atStartOfDay());
                         if (!exportableNetexData.getSharedOperatingPeriods().containsKey(operatingPeriodId)) {
                             exportableNetexData.getSharedOperatingPeriods().put(operatingPeriodId, operatingPeriod);
-                        } else {
-                            exportableNetexData.getSharedOperatingPeriods().put(operatingPeriodId + "-" + i, operatingPeriod);
                         }
 
                         NetexProducerUtils.populateReference(operatingPeriod, operatingPeriodRef, true);
