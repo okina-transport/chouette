c761c28a29fd72299b42d66deb9ee316d7f979f9
poswald <poswald@okina.fr>
2025-02-11 14:32:12 +0100

    Revert "feat : DATA-785 anomaly on OperatingPeriod ToDate tag schedules in Netex data"
    This reverts commit 16eca3bf497a83138210c2c5af7f755d8ba7e516.

--- mobi.chouette.exchange.netexprofile/src/main/java/mobi/chouette/exchange/netexprofile/exporter/writer/NetexCalendrierWriter.java
+++ mobi.chouette.exchange.netexprofile/src/main/java/mobi/chouette/exchange/netexprofile/exporter/writer/NetexCalendrierWriter.java
@@ -19,59 +19,55 @@ import static mobi.chouette.exchange.netexprofile.util.NetexObjectIdTypes.MEMBER
 
 public class NetexCalendrierWriter extends AbstractNetexWriter {
 
-	public static void writer(XMLStreamWriter writer, Context context, ExportableNetexData exportableNetexData, Marshaller marshaller, String timestamp, String typeNetex) throws XMLStreamException, JAXBException {
-		writer.writeAttribute(ID, NetexProducerUtils.createUniqueGeneralFrameId(context, GENERAL_FRAME, typeNetex, timestamp));
+    public static void writer(XMLStreamWriter writer, Context context, ExportableNetexData exportableNetexData, Marshaller marshaller, String timestamp, String typeNetex) throws XMLStreamException, JAXBException {
+        writer.writeAttribute(ID,  NetexProducerUtils.createUniqueGeneralFrameId(context, GENERAL_FRAME, typeNetex, timestamp));
 
-		ValidBetween validBetween = new ValidBetween();
-		LocalDateTime fromDate;
-		LocalDateTime toDate;
+        ValidBetween validBetween = new ValidBetween();
+        LocalDateTime fromDate;
+        LocalDateTime toDate;
 
 
-		if (exportableNetexData.getSharedOperatingPeriods().size() > 0) {
-			//At least one calendar is existing.Valid periods are recovered from the calendar
+        if (exportableNetexData.getSharedOperatingPeriods().size() > 0){
+            //At least one calendar is existing.Valid periods are recovered from the calendar
 
-			fromDate = exportableNetexData.getSharedOperatingPeriods().values()
-					.stream()
-					.map(OperatingPeriod_VersionStructure::getFromDate)
-					.min(LocalDateTime::compareTo)
-					.get();
+            fromDate = exportableNetexData.getSharedOperatingPeriods().values()
+                    .stream()
+                    .map(OperatingPeriod_VersionStructure::getFromDate)
+                    .min(LocalDateTime::compareTo)
+                    .get();
 
-			toDate = exportableNetexData.getSharedOperatingPeriods().values()
-					.stream()
-					.map(OperatingPeriod_VersionStructure::getToDate)
-					.max(LocalDateTime::compareTo)
-					.get();
+            toDate = exportableNetexData.getSharedOperatingPeriods().values()
+                    .stream()
+                    .map(OperatingPeriod_VersionStructure::getToDate)
+                    .max(LocalDateTime::compareTo)
+                    .get();
 
-		} else {
-			//there is no calendar. Only inclusion dates. Valid periods are recovered from inclusion dates
-			List<LocalDateTime> inclusionDateList = exportableNetexData.getSharedDayTypeAssignments().stream()
-					.map(dayTypeAssignment -> dayTypeAssignment.getDate())
-					.collect(Collectors.toList());
+        }else{
+            //there is no calendar. Only inclusion dates. Valid periods are recovered from inclusion dates
+            List<LocalDateTime> inclusionDateList = exportableNetexData.getSharedDayTypeAssignments().stream()
+                                                                                                     .map(dayTypeAssignment -> dayTypeAssignment.getDate())
+                                                                                                     .collect(Collectors.toList());
 
-			fromDate = inclusionDateList.stream()
-					.min(LocalDateTime::compareTo)
-					.get();
+            fromDate = inclusionDateList.stream()
+                                        .min(LocalDateTime::compareTo)
+                                        .get();
 
-			toDate = inclusionDateList.stream()
-					.max(LocalDateTime::compareTo)
-					.get();
-		}
+            toDate = inclusionDateList.stream()
+                                      .max(LocalDateTime::compareTo)
+                                      .get();
+        }
 
-		if (toDate.getHour() == 0 && toDate.getMinute() == 0) {
-			// Mettre à jour l'heure et les minutes
-			toDate = toDate.withHour(23).withMinute(59);
-		}
+        validBetween.setFromDate(fromDate);
+        validBetween.setToDate(toDate);
 
-		validBetween.setToDate(toDate);
+        marshaller.marshal(validBetween, writer);
 
-		marshaller.marshal(validBetween, writer);
+        TypeOfFrameWriter.typeOfFrameWriter(writer, marshaller, typeNetex);
 
-		TypeOfFrameWriter.typeOfFrameWriter(writer, marshaller, typeNetex);
+        writer.writeStartElement(MEMBERS);
 
-		writer.writeStartElement(MEMBERS);
+        ServiceCalendarFrameFranceWriter.write(writer, context, exportableNetexData, marshaller);
 
-		ServiceCalendarFrameFranceWriter.write(writer, context, exportableNetexData, marshaller);
-
-		writer.writeEndElement();
-	}
+        writer.writeEndElement();
+    }
 }
