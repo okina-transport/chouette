afa1be483390832a563abf74738d6182fb4ba82d
gcarrere <gcarrere@okina.fr>
2025-02-26 16:51:24 +0100

    generate 5.4.4-2

--- build.sh
+++ build.sh
@@ -3,12 +3,8 @@
 # Version de l'image de base. Décorellé de la version applicative, n'évolue pas souvent.
 CHOUETTE_BASE_VERSION=1.5
 
-MVN_VERSION=$(mvn -q \
-    -Dexec.executable=echo \
-    -Dexec.args='${project.version}' \
-    --non-recursive \
-    exec:exec)
-
+MVN_VERSION=$(mvn help:evaluate -Dexpression=project.version -q -DforceStdout)
+MVN_VERSION=`echo ${MVN_VERSION} | sed -r "s/\x1B\[([0-9]{1,3}(;[0-9]{1,2};?)?)?[mGK]//g"`
 
 #mvn  install -DskipTests -DskipWildfly=true -DskipInitDb=true
 docker build -t registry.okina.fr/mobiiti/chouette:${MVN_VERSION} -f docker/Dockerfile-build --build-arg CHOUETTE_BASE_VERSION=registry.okina.fr/mobiiti/chouette-base:${CHOUETTE_BASE_VERSION} .
