c4c6788c84d7566cfd5ab1de7b0524187a91b415
poswald <poswald@okina.fr>
2025-03-24 13:19:42 +0100

    fix : gtfs export fares ids

--- mobi.chouette.exchange.gtfs/src/main/java/mobi/chouette/exchange/gtfs/exporter/GtfsStopUtils.java
+++ mobi.chouette.exchange.gtfs/src/main/java/mobi/chouette/exchange/gtfs/exporter/GtfsStopUtils.java
@@ -9,66 +9,63 @@ import org.apache.commons.lang.StringUtils;
 
 public class GtfsStopUtils {
 
-    private static final String TRIDENT_STOP_PLACE_TYPE=":StopPlace:";
-    private static final String TRIDENT_QUAY_TYPE=":Quay:";
+	private static final String TRIDENT_STOP_PLACE_TYPE = ":StopPlace:";
+	private static final String TRIDENT_QUAY_TYPE = ":Quay:";
 
 
-
-    public static String getNewStopId(StopArea stop, IdParameters idParams, boolean keepOriginalId, String schemaPrefix) {
-        String idPrefix = ChouetteAreaEnum.BoardingPosition.equals(stop.getAreaType()) ? idParams.getStopIdPrefix() : idParams.getCommercialPointIdPrefix();
-        if (!keepOriginalId && StringUtils.isNotEmpty(stop.getOriginalStopId())) {
-            if (IdFormat.TRIDENT.equals(idParams.getIdFormat()) && StringUtils.isNotEmpty(idPrefix)) {
-                return createTridentId(stop, idPrefix);
-            }
-            if (IdFormat.SOURCE.equals(idParams.getIdFormat())) {
-                return createStandardId(stop, idPrefix);
-            }
-        }
-        if (keepOriginalId && IdFormat.SOURCE_GLOBAL.equals(idParams.getIdFormat())) {
-            return createTridentIdStandard(stop, schemaPrefix);
-        }
-        return stop.getObjectId();
-    }
-
-
-    /**
-     * Creates a new standard ID
-     * e.g. : PREFIXxxxx
-     * @param stop
-     *       Stop for which a trident Id must be generated
-     * @param idPrefix
-     *       Prefix that will be used on Id beginning.
-     * @return
-     */
-    private static String createStandardId(StopArea stop, String idPrefix){
-        String originalId = stop.getOriginalStopId().replace(Constant.COLON_REPLACEMENT_CODE, ":");
-        return StringUtils.isEmpty(idPrefix) ? originalId : idPrefix + originalId;
-    }
-
-    /**
-     * Creates a new trident ID depending on the object type : Quay or StopPlace
-     * e.g. : PREFIX:Quay:10545 or PREFIX:StopPlace:10545
-     * @param stop
-     *       Stop for which a trident Id must be generated
-     * @param idPrefix
-     *       Prefix that will be used on Id beginning.
-     * @return
-     */
-    private static String createTridentId(StopArea stop, String idPrefix) {
-        String originalId = stop.getOriginalStopId().replace(Constant.COLON_REPLACEMENT_CODE, ":");
-        if (stop.getObjectId().contains("Quay")) {
-            return idPrefix + TRIDENT_QUAY_TYPE + originalId;
-        }
-        return idPrefix + TRIDENT_STOP_PLACE_TYPE + originalId;
-    }
+	public static String getNewStopId(StopArea stop, IdParameters idParams, boolean keepOriginalId, String schemaPrefix) {
+		String idPrefix = ChouetteAreaEnum.BoardingPosition.equals(stop.getAreaType()) ? idParams.getStopIdPrefix() : idParams.getCommercialPointIdPrefix();
+		if (IdFormat.TRIDENT.equals(idParams.getIdFormat())) {
+			if (StringUtils.isNotEmpty(idPrefix)) {
+				return createTridentId(stop, idPrefix);
+			} else if (schemaPrefix != null) {
+				return createTridentId(stop, schemaPrefix);
+			}
+		} else if (IdFormat.SOURCE.equals(idParams.getIdFormat())) {
+			return createStandardId(stop, idPrefix);
+		} else if (keepOriginalId && IdFormat.SOURCE_GLOBAL.equals(idParams.getIdFormat())) {
+			return createTridentIdStandard(stop, schemaPrefix);
+		}
+		return stop.getObjectId();
+	}
 
 
-    private static String createTridentIdStandard(StopArea stop, String prefix) {
-        String originalId = stop.getOriginalStopId().replace(Constant.COLON_REPLACEMENT_CODE, ":");
-        if (stop.getObjectId().contains("Quay")) {
-            return prefix + TRIDENT_QUAY_TYPE + originalId;
-        }
-        return prefix + TRIDENT_STOP_PLACE_TYPE + originalId;
-    }
+	/**
+	 * Creates a new standard ID
+	 * e.g. : PREFIXxxxx
+	 *
+	 * @param stop     Stop for which a trident Id must be generated
+	 * @param idPrefix Prefix that will be used on Id beginning.
+	 * @return
+	 */
+	private static String createStandardId(StopArea stop, String idPrefix) {
+		String originalId = stop.getOriginalStopId().replace(Constant.COLON_REPLACEMENT_CODE, ":");
+		return StringUtils.isEmpty(idPrefix) ? originalId : idPrefix + originalId;
+	}
+
+	/**
+	 * Creates a new trident ID depending on the object type : Quay or StopPlace
+	 * e.g. : PREFIX:Quay:10545 or PREFIX:StopPlace:10545
+	 *
+	 * @param stop     Stop for which a trident Id must be generated
+	 * @param idPrefix Prefix that will be used on Id beginning.
+	 * @return
+	 */
+	private static String createTridentId(StopArea stop, String idPrefix) {
+		String originalId = stop.getOriginalStopId().replace(Constant.COLON_REPLACEMENT_CODE, ":");
+		if (stop.getObjectId().contains("Quay")) {
+			return idPrefix + TRIDENT_QUAY_TYPE + originalId;
+		}
+		return idPrefix + TRIDENT_STOP_PLACE_TYPE + originalId;
+	}
+
+
+	private static String createTridentIdStandard(StopArea stop, String prefix) {
+		String originalId = stop.getOriginalStopId().replace(Constant.COLON_REPLACEMENT_CODE, ":");
+		if (stop.getObjectId().contains("Quay")) {
+			return prefix + TRIDENT_QUAY_TYPE + originalId;
+		}
+		return prefix + TRIDENT_STOP_PLACE_TYPE + originalId;
+	}
 
 }
