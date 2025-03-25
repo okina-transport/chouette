645ccb6f11d872f3cf306bd3d9dbd7a65ff444ce
poswald <poswald@okina.fr>
2025-03-25 16:59:59 +0100

    feat : DATA-942 prefix deletion on import does not work with fare option

--- mobi.chouette.exchange.gtfs/src/main/java/mobi/chouette/exchange/gtfs/parser/GtfsFareRuleParser.java
+++ mobi.chouette.exchange.gtfs/src/main/java/mobi/chouette/exchange/gtfs/parser/GtfsFareRuleParser.java
@@ -23,111 +23,118 @@ import mobi.chouette.model.util.Referential;
 @Log4j
 public class GtfsFareRuleParser implements Parser, Validator, Constant {
 
-    static {
-        ParserFactory.register(GtfsFareRuleParser.class.getName(), new ParserFactory() {
-            @Override
-            protected Parser create() {
-                return new GtfsFareRuleParser();
-            }
-        });
-    }
+	static {
+		ParserFactory.register(GtfsFareRuleParser.class.getName(), new ParserFactory() {
+			@Override
+			protected Parser create() {
+				return new GtfsFareRuleParser();
+			}
+		});
+	}
 
-    @Getter
-    @Setter
-    private String gtfsFareId;
+	@Getter
+	@Setter
+	private String gtfsFareId;
 
-    @Override
-    public void validate(Context context) throws Exception {
-        GtfsImporter importer = (GtfsImporter) context.get(PARSER);
-        GtfsValidationReporter gtfsValidationReporter = (GtfsValidationReporter) context.get(GTFS_REPORTER);
-        gtfsValidationReporter.getExceptions().clear();
+	@Getter
+	@Setter
+	private String gtfsRouteId;
 
-        // fare_attributes.txt
-        if (importer.hasFareRuleImporter()) { // the file "fare_attributes.txt" exists ?
-            gtfsValidationReporter.reportSuccess(context, GTFS_1_GTFS_Common_1, GTFS_FARE_ATTRIBUTES_FILE);
+	@Override
+	public void validate(Context context) throws Exception {
+		GtfsImporter importer = (GtfsImporter) context.get(PARSER);
+		GtfsValidationReporter gtfsValidationReporter = (GtfsValidationReporter) context.get(GTFS_REPORTER);
+		gtfsValidationReporter.getExceptions().clear();
 
-            Index<GtfsFareRule> parser = null;
-            try {
-                parser = importer.getFareRuleById();
-            } catch (Exception ex) {
-                if (ex instanceof GtfsException) {
-                    gtfsValidationReporter.reportError(context, (GtfsException) ex, GTFS_FARE_ATTRIBUTES_FILE);
-                } else {
-                    gtfsValidationReporter.throwUnknownError(context, ex, GTFS_FARE_ATTRIBUTES_FILE);
-                }
-            }
+		// fare_attributes.txt
+		if (importer.hasFareRuleImporter()) { // the file "fare_attributes.txt" exists ?
+			gtfsValidationReporter.reportSuccess(context, GTFS_1_GTFS_Common_1, GTFS_FARE_RULES_FILE);
 
-            gtfsValidationReporter.validateOkCSV(context, GTFS_FARE_ATTRIBUTES_FILE);
+			Index<GtfsFareRule> parser = null;
+			try {
+				parser = importer.getFareRuleById();
+			} catch (Exception ex) {
+				if (ex instanceof GtfsException) {
+					gtfsValidationReporter.reportError(context, (GtfsException) ex, GTFS_FARE_RULES_FILE);
+				} else {
+					gtfsValidationReporter.throwUnknownError(context, ex, GTFS_FARE_RULES_FILE);
+				}
+			}
 
-            if (parser == null) { // importer.getFareRuleById() fails for any other reason
-                gtfsValidationReporter.throwUnknownError(context, new Exception("Cannot instantiate FareRuleById class"), GTFS_FARE_ATTRIBUTES_FILE);
-            } else {
-                gtfsValidationReporter.validate(context, GTFS_FARE_ATTRIBUTES_FILE, parser.getOkTests());
-                gtfsValidationReporter.validateUnknownError(context);
-            }
+			gtfsValidationReporter.validateOkCSV(context, GTFS_FARE_RULES_FILE);
 
-            if (!parser.getErrors().isEmpty()) {
-                gtfsValidationReporter.reportErrors(context, parser.getErrors(), GTFS_FARE_ATTRIBUTES_FILE);
-                parser.getErrors().clear();
-            }
+			if (parser == null) { // importer.getFareRuleById() fails for any other reason
+				gtfsValidationReporter.throwUnknownError(context, new Exception("Cannot instantiate FareRuleById class"), GTFS_FARE_RULES_FILE);
+			} else {
+				gtfsValidationReporter.validate(context, GTFS_FARE_RULES_FILE, parser.getOkTests());
+				gtfsValidationReporter.validateUnknownError(context);
+			}
 
-            gtfsValidationReporter.validateOKGeneralSyntax(context, GTFS_FARE_ATTRIBUTES_FILE);
+			if (!parser.getErrors().isEmpty()) {
+				gtfsValidationReporter.reportErrors(context, parser.getErrors(), GTFS_FARE_RULES_FILE);
+				parser.getErrors().clear();
+			}
 
-            if (parser.getLength() == 0) {
-                gtfsValidationReporter.reportError(context, new GtfsException(GTFS_FARE_ATTRIBUTES_FILE, 1, null, GtfsException.ERROR.OPTIONAL_FILE_WITH_NO_ENTRY, null, null), GTFS_FARE_ATTRIBUTES_FILE);
-            } else {
-                gtfsValidationReporter.validate(context, GTFS_FARE_ATTRIBUTES_FILE, GtfsException.ERROR.FILE_WITH_NO_ENTRY);
-            }
+			gtfsValidationReporter.validateOKGeneralSyntax(context, GTFS_FARE_RULES_FILE);
 
-            GtfsException fatalException = null;
-            parser.setWithValidation(true);
-            for (GtfsFareRule bean : parser) {
-                try {
-                    parser.validate(bean, importer);
-                } catch (Exception ex) {
-                    if (ex instanceof GtfsException) {
-                        gtfsValidationReporter.reportError(context, (GtfsException) ex, GTFS_FARE_ATTRIBUTES_FILE);
-                    } else {
-                        gtfsValidationReporter.throwUnknownError(context, ex, GTFS_FARE_ATTRIBUTES_FILE);
-                    }
-                }
-                for (GtfsException ex : bean.getErrors()) {
-                    if (ex.isFatal())
-                        fatalException = ex;
-                }
-                gtfsValidationReporter.reportErrors(context, bean.getErrors(), GTFS_FARE_ATTRIBUTES_FILE);
-                gtfsValidationReporter.validate(context, GTFS_FARE_ATTRIBUTES_FILE, bean.getOkTests());
-            }
-            parser.setWithValidation(false);
-            if (fatalException != null)
-                throw fatalException;
-        } else {
-            gtfsValidationReporter.reportError(context, new GtfsException(GTFS_FARE_ATTRIBUTES_FILE, 1, null, GtfsException.ERROR.MISSING_OPTIONAL_FILE, null, null), GTFS_FARE_ATTRIBUTES_FILE);
-        }
-    }
+			if (parser.getLength() == 0) {
+				gtfsValidationReporter.reportError(context, new GtfsException(GTFS_FARE_RULES_FILE, 1, null, GtfsException.ERROR.OPTIONAL_FILE_WITH_NO_ENTRY, null, null), GTFS_FARE_RULES_FILE);
+			} else {
+				gtfsValidationReporter.validate(context, GTFS_FARE_RULES_FILE, GtfsException.ERROR.FILE_WITH_NO_ENTRY);
+			}
 
-    @Override
-    public void parse(Context context) throws Exception {
+			GtfsException fatalException = null;
+			parser.setWithValidation(true);
+			for (GtfsFareRule bean : parser) {
+				try {
+					GtfsImportParameters parameters = (GtfsImportParameters) context.get(CONFIGURATION);
+					bean.setRouteId(bean.getRouteId().replaceFirst("^" + parameters.getLinePrefixToRemove(), ""));
+					parser.validate(bean, importer);
+				} catch (Exception ex) {
+					if (ex instanceof GtfsException) {
+						gtfsValidationReporter.reportError(context, (GtfsException) ex, GTFS_FARE_RULES_FILE);
+					} else {
+						gtfsValidationReporter.throwUnknownError(context, ex, GTFS_FARE_RULES_FILE);
+					}
+				}
+				for (GtfsException ex : bean.getErrors()) {
+					if (ex.isFatal())
+						fatalException = ex;
+				}
+				gtfsValidationReporter.reportErrors(context, bean.getErrors(), GTFS_FARE_RULES_FILE);
+				gtfsValidationReporter.validate(context, GTFS_FARE_RULES_FILE, bean.getOkTests());
+			}
+			parser.setWithValidation(false);
+			if (fatalException != null)
+				throw fatalException;
+		} else {
+			gtfsValidationReporter.reportError(context, new GtfsException(GTFS_FARE_RULES_FILE, 1, null, GtfsException.ERROR.MISSING_OPTIONAL_FILE, null, null), GTFS_FARE_RULES_FILE);
+		}
+	}
 
-        Referential referential = (Referential) context.get(REFERENTIAL);
-        GtfsImporter importer = (GtfsImporter) context.get(PARSER);
+	@Override
+	public void parse(Context context) throws Exception {
 
-        for (GtfsFareRule gtfsFareRule : importer.getFareRuleById()) {
-            GtfsImportParameters configuration = (GtfsImportParameters) context.get(CONFIGURATION);
-            String id = ObjectIdUtil.composeNeptuneObjectId(configuration.getObjectIdPrefix(), FareRule.FARE_KEY, gtfsFareRule.getFareId());
-            FareRule fareRule = ObjectFactory.getFareRule(referential, id);
+		Referential referential = (Referential) context.get(REFERENTIAL);
+		GtfsImporter importer = (GtfsImporter) context.get(PARSER);
 
-            String routeId = ObjectIdUtil.composeNeptuneObjectId(configuration.getObjectIdPrefix(), FareRule.ROUTE_KEY, gtfsFareRule.getRouteId());
-            Route route = ObjectFactory.getRoute(referential, routeId);
-            convert(gtfsFareRule, fareRule, route);
-        }
-    }
+		for (GtfsFareRule gtfsFareRule : importer.getFareRuleById()) {
+			GtfsImportParameters configuration = (GtfsImportParameters) context.get(CONFIGURATION);
+			String id = ObjectIdUtil.composeNeptuneObjectId(configuration.getObjectIdPrefix(), FareRule.FARE_KEY, gtfsFareRule.getFareId());
+			FareRule fareRule = ObjectFactory.getFareRule(referential, id);
 
-    protected void convert(GtfsFareRule gtfsFareRule, FareRule fareRule, Route route) throws Exception {
-        fareRule.setRoute(route);
-        fareRule.setOriginId(gtfsFareRule.getOriginId());
-        fareRule.setDestinationId(gtfsFareRule.getDestinationId());
-        fareRule.setContainsId(gtfsFareRule.getContainsId());
-    }
+			String routeIdModified = gtfsFareRule.getRouteId().replaceFirst("^" + configuration.getLinePrefixToRemove(), "");
+			String routeId = ObjectIdUtil.composeNeptuneObjectId(configuration.getObjectIdPrefix(), FareRule.ROUTE_KEY, routeIdModified);
+			Route route = ObjectFactory.getRoute(referential, routeId);
+			convert(gtfsFareRule, fareRule, route);
+		}
+	}
+
+	protected void convert(GtfsFareRule gtfsFareRule, FareRule fareRule, Route route) throws Exception {
+		fareRule.setRoute(route);
+		fareRule.setOriginId(gtfsFareRule.getOriginId());
+		fareRule.setDestinationId(gtfsFareRule.getDestinationId());
+		fareRule.setContainsId(gtfsFareRule.getContainsId());
+	}
 
 }
--- mobi.chouette.exchange/src/main/java/mobi/chouette/exchange/importer/FareRuleRegisterCommand.java
+++ mobi.chouette.exchange/src/main/java/mobi/chouette/exchange/importer/FareRuleRegisterCommand.java
@@ -36,117 +36,118 @@ import java.sql.SQLException;
 @Stateless(name = FareRuleRegisterCommand.COMMAND)
 public class FareRuleRegisterCommand implements Command {
 
-    public static final String COMMAND = "FareRuleRegisterCommand";
+	public static final String COMMAND = "FareRuleRegisterCommand";
 
-    static {
-        CommandFactory.factories.put(FareRuleRegisterCommand.class.getName(), new DefaultCommandFactory());
-    }
+	static {
+		CommandFactory.factories.put(FareRuleRegisterCommand.class.getName(), new DefaultCommandFactory());
+	}
 
-    @EJB
-    private FareRuleOptimiser optimiser;
+	@EJB
+	private FareRuleOptimiser optimiser;
 
-    @EJB
-    private FareRuleDAO fareRuleDAO;
+	@EJB
+	private FareRuleDAO fareRuleDAO;
 
-    @EJB
-    private RouteDAO routeDAO;
+	@EJB
+	private RouteDAO routeDAO;
 
-    @EJB(beanName = FareRuleUpdater.BEAN_NAME)
-    private Updater<FareRule> fareRuleUpdater;
+	@EJB(beanName = FareRuleUpdater.BEAN_NAME)
+	private Updater<FareRule> fareRuleUpdater;
 
-    @EJB(beanName = RouteUpdater.BEAN_NAME)
-    private Updater<Route> routeUpdater;
+	@EJB(beanName = RouteUpdater.BEAN_NAME)
+	private Updater<Route> routeUpdater;
 
-    @Override
-    @TransactionAttribute(TransactionAttributeType.REQUIRES_NEW)
-    public boolean execute(Context context) throws Exception {
+	@Override
+	@TransactionAttribute(TransactionAttributeType.REQUIRES_NEW)
+	public boolean execute(Context context) throws Exception {
 
-        boolean result = ERROR;
-        Monitor monitor = MonitorFactory.start(COMMAND);
+		boolean result = ERROR;
+		Monitor monitor = MonitorFactory.start(COMMAND);
 
-        if (!context.containsKey(OPTIMIZED)) {
-            context.put(OPTIMIZED, Boolean.TRUE);
-        }
-        Referential cache = new Referential();
-        context.put(CACHE, cache);
+		if (!context.containsKey(OPTIMIZED)) {
+			context.put(OPTIMIZED, Boolean.TRUE);
+		}
+		Referential cache = new Referential();
+		context.put(CACHE, cache);
 
-        Referential referential = (Referential) context.get(REFERENTIAL);
+		Referential referential = (Referential) context.get(REFERENTIAL);
 
-        // Use property based enabling of stop place updater, but allow disabling if property exist in context
-        FareRule newValue = referential.getFareRules().values().iterator().next();
-        context.put(CURRENT_FARE_RULE_ID, newValue.getObjectId());
+		// Use property based enabling of stop place updater, but allow disabling if property exist in context
+		FareRule newValue = referential.getFareRules().values().iterator().next();
+		context.put(CURRENT_FARE_RULE_ID, newValue.getObjectId());
 
-        log.info("register fare rule : " + newValue.getObjectId());
-        try {
-            optimiser.initialize(cache, referential);
+		log.info("register fare rule : " + newValue.getObjectId());
+		try {
+			optimiser.initialize(cache, referential);
 
-            FareRule oldValue = cache.getFareRules().get(newValue.getObjectId());
-            Route oldRouteValue = cache.getRoutes().get(newValue.getRoute().getObjectId());
+			FareRule oldValue = cache.getFareRules().get(newValue.getObjectId());
+			Route oldRouteValue = cache.getRoutes().get(newValue.getRoute().getObjectId());
 
-            Route findedRoute = routeDAO.findByObjectId(oldRouteValue.getObjectId());
+			Route findedRoute = routeDAO.findByObjectId(oldRouteValue.getObjectId());
 
-            if (oldValue.getId() == null && findedRoute == null) {
-                routeDAO.create(oldRouteValue);
-            } else if (findedRoute != null) {
-                routeUpdater.update(context, findedRoute, oldRouteValue);
-            }
+			if (oldValue.getId() == null && findedRoute == null) {
+				routeDAO.create(oldRouteValue);
+			} else if (findedRoute != null) {
+				routeUpdater.update(context, findedRoute, oldRouteValue);
+			}
 
-            newValue.setRoute(oldRouteValue);
+			Route findedRouteAfterSave = routeDAO.findByObjectId(oldRouteValue.getObjectId());
+			oldValue.setRoute(findedRouteAfterSave);
 
-            if (oldValue.getId() == null) {
-                fareRuleDAO.create(oldValue);
-            } else {
-                fareRuleUpdater.update(context, oldValue, newValue);
-            }
-            fareRuleDAO.flush();
+			if (oldValue.getId() == null) {
+				fareRuleDAO.create(oldValue);
+			} else {
+				fareRuleUpdater.update(context, oldValue, newValue);
+			}
+			fareRuleDAO.flush();
 
-            result = SUCCESS;
-        } catch (Exception ex) {
-            log.error(ex.getMessage());
-            ActionReporter reporter = ActionReporter.Factory.getInstance();
-            reporter.addObjectReport(context, newValue.getObjectId(),
-                    OBJECT_TYPE.FARE_RULE, NamingUtil.getName(newValue), OBJECT_STATE.ERROR, IO_TYPE.INPUT);
-            if (ex.getCause() != null) {
-                Throwable e = ex.getCause();
-                while (e.getCause() != null) {
-                    log.error(e.getMessage());
-                    e = e.getCause();
-                }
-                if (e instanceof SQLException) {
-                    e = ((SQLException) e).getNextException();
-                    reporter.addErrorToObjectReport(context, newValue.getObjectId(), OBJECT_TYPE.FARE_RULE, ERROR_CODE.WRITE_ERROR, e.getMessage());
+			result = SUCCESS;
+		} catch (Exception ex) {
+			log.error(ex.getMessage());
+			ActionReporter reporter = ActionReporter.Factory.getInstance();
+			reporter.addObjectReport(context, newValue.getObjectId(),
+					OBJECT_TYPE.FARE_RULE, NamingUtil.getName(newValue), OBJECT_STATE.ERROR, IO_TYPE.INPUT);
+			if (ex.getCause() != null) {
+				Throwable e = ex.getCause();
+				while (e.getCause() != null) {
+					log.error(e.getMessage());
+					e = e.getCause();
+				}
+				if (e instanceof SQLException) {
+					e = ((SQLException) e).getNextException();
+					reporter.addErrorToObjectReport(context, newValue.getObjectId(), OBJECT_TYPE.FARE_RULE, ERROR_CODE.WRITE_ERROR, e.getMessage());
 
-                } else {
-                    reporter.addErrorToObjectReport(context, newValue.getObjectId(), OBJECT_TYPE.FARE_RULE, ERROR_CODE.INTERNAL_ERROR, e.getMessage());
-                }
-            } else {
-                reporter.addErrorToObjectReport(context, newValue.getObjectId(), OBJECT_TYPE.FARE_RULE, ERROR_CODE.INTERNAL_ERROR, ex.getMessage());
-            }
-            throw ex;
-        } finally {
-            log.info(Color.MAGENTA + monitor.stop() + Color.NORMAL);
-        }
-        return result;
-    }
+				} else {
+					reporter.addErrorToObjectReport(context, newValue.getObjectId(), OBJECT_TYPE.FARE_RULE, ERROR_CODE.INTERNAL_ERROR, e.getMessage());
+				}
+			} else {
+				reporter.addErrorToObjectReport(context, newValue.getObjectId(), OBJECT_TYPE.FARE_RULE, ERROR_CODE.INTERNAL_ERROR, ex.getMessage());
+			}
+			throw ex;
+		} finally {
+			log.info(Color.MAGENTA + monitor.stop() + Color.NORMAL);
+		}
+		return result;
+	}
 
-    public static class DefaultCommandFactory extends CommandFactory {
+	public static class DefaultCommandFactory extends CommandFactory {
 
-        @Override
-        protected Command create(InitialContext context) throws IOException {
-            Command result = null;
-            try {
-                String name = "java:app/mobi.chouette.exchange/" + COMMAND;
-                result = (Command) context.lookup(name);
-            } catch (NamingException e) {
-                // try another way on test context
-                String name = "java:module/" + COMMAND;
-                try {
-                    result = (Command) context.lookup(name);
-                } catch (NamingException e1) {
-                    log.error(e);
-                }
-            }
-            return result;
-        }
-    }
+		@Override
+		protected Command create(InitialContext context) throws IOException {
+			Command result = null;
+			try {
+				String name = "java:app/mobi.chouette.exchange/" + COMMAND;
+				result = (Command) context.lookup(name);
+			} catch (NamingException e) {
+				// try another way on test context
+				String name = "java:module/" + COMMAND;
+				try {
+					result = (Command) context.lookup(name);
+				} catch (NamingException e1) {
+					log.error(e);
+				}
+			}
+			return result;
+		}
+	}
 }
