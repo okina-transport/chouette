7182955474571e82951050e6cce701851e5d5202
poswald <poswald@okina.fr>
2025-11-04 09:02:40 +0100

    Reapply "fix: DATA 1302 netex sncf without quays TUs"
    This reverts commit 1666bf6585b38dafe427e92e089cf376ad3cb1f3.

--- mobi.chouette.exchange.netexprofile/src/test/java/mobi/chouette/exchange/netexprofile/parser/DestinationDisplayParserTest.java
+++ mobi.chouette.exchange.netexprofile/src/test/java/mobi/chouette/exchange/netexprofile/parser/DestinationDisplayParserTest.java
@@ -4,6 +4,7 @@ import static mobi.chouette.common.Constant.REFERENTIAL;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashSet;
 
 import javax.xml.bind.JAXBException;
@@ -74,6 +75,7 @@ public class DestinationDisplayParserTest {
 		PublicationDeliveryStructure pubDelivery = netexImporter.unmarshal(new File(netedFilePath), new HashSet<>());
 
 		context.put(Constant.NETEX_DATA_JAVA, pubDelivery);
+		context.put(Constant.INCOMING_LINE_LIST, new ArrayList());
 
 		parser.parse(context);
 	}
