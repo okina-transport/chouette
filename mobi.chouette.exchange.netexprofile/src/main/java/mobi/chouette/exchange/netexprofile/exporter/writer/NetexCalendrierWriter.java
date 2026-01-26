af78e347b34c4badae9b583c1cd668323824aa33
poswald <poswald@okina.fr>
2026-01-26 14:53:24 +0100

    fix: DATA-1646 DayTypeAssignments are not taken into account in overall validity

--- mobi.chouette.exchange.netexprofile/src/main/java/mobi/chouette/exchange/netexprofile/exporter/writer/NetexCalendrierWriter.java
+++ mobi.chouette.exchange.netexprofile/src/main/java/mobi/chouette/exchange/netexprofile/exporter/writer/NetexCalendrierWriter.java
@@ -3,9 +3,9 @@ package mobi.chouette.exchange.netexprofile.exporter.writer;
 import mobi.chouette.common.Context;
 import mobi.chouette.exchange.netexprofile.exporter.ExportableNetexData;
 import mobi.chouette.exchange.netexprofile.exporter.producer.NetexProducerUtils;
+import org.apache.commons.collections.CollectionUtils;
 import org.apache.commons.collections.MapUtils;
 import org.rutebanken.netex.model.DayTypeAssignment_VersionStructure;
-import org.rutebanken.netex.model.OperatingPeriod_VersionStructure;
 import org.rutebanken.netex.model.ValidBetween;
 
 import javax.xml.bind.JAXBException;
@@ -13,8 +13,9 @@ import javax.xml.bind.Marshaller;
 import javax.xml.stream.XMLStreamException;
 import javax.xml.stream.XMLStreamWriter;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
+import java.util.Objects;
 
 import static mobi.chouette.exchange.netexprofile.util.NetexObjectIdTypes.GENERAL_FRAME;
 import static mobi.chouette.exchange.netexprofile.util.NetexObjectIdTypes.MEMBERS;
@@ -24,52 +25,50 @@ public class NetexCalendrierWriter extends AbstractNetexWriter {
     public static void writer(XMLStreamWriter writer, Context context, ExportableNetexData exportableNetexData, Marshaller marshaller, String timestamp, String typeNetex) throws XMLStreamException, JAXBException {
         writer.writeAttribute(ID, NetexProducerUtils.createUniqueGeneralFrameId(context, GENERAL_FRAME, typeNetex, timestamp));
 
-        ValidBetween validBetween = new ValidBetween();
-        LocalDateTime fromDate;
-        LocalDateTime toDate;
-
+        List<LocalDateTime> allDates = new ArrayList<>();
 
         if (MapUtils.isNotEmpty(exportableNetexData.getSharedOperatingPeriods())) {
-            //At least one calendar is existing.Valid periods are recovered from the calendar
-
-            fromDate = exportableNetexData.getSharedOperatingPeriods().values()
-                    .stream()
-                    .map(OperatingPeriod_VersionStructure::getFromDate)
-                    .min(LocalDateTime::compareTo)
-                    .get();
-
-            toDate = exportableNetexData.getSharedOperatingPeriods().values()
-                    .stream()
-                    .map(OperatingPeriod_VersionStructure::getToDate)
-                    .max(LocalDateTime::compareTo)
-                    .get();
-
-        } else {
-            //there is no calendar. Only inclusion dates. Valid periods are recovered from inclusion dates
-            List<LocalDateTime> inclusionDateList = exportableNetexData.getSharedDayTypeAssignments().stream()
-                    .map(DayTypeAssignment_VersionStructure::getDate)
-                    .collect(Collectors.toList());
-
-            fromDate = inclusionDateList.stream()
-                    .min(LocalDateTime::compareTo)
-                    .get();
-
-            toDate = inclusionDateList.stream()
-                    .max(LocalDateTime::compareTo)
-                    .get();
+            exportableNetexData.getSharedOperatingPeriods().values().forEach(op -> {
+                if (op.getFromDate() != null) {
+					allDates.add(op.getFromDate());
+				}
+                if (op.getToDate() != null) {
+					allDates.add(op.getToDate());
+				}
+            });
         }
 
-        validBetween.setFromDate(fromDate);
-        validBetween.setToDate(toDate);
+        if (CollectionUtils.isNotEmpty(exportableNetexData.getSharedDayTypeAssignments())) {
+            exportableNetexData.getSharedDayTypeAssignments().stream()
+                    .map(DayTypeAssignment_VersionStructure::getDate)
+                    .filter(Objects::nonNull)
+                    .forEach(allDates::add);
+        }
 
-        marshaller.marshal(validBetween, writer);
+        if (CollectionUtils.isNotEmpty(allDates)) {
+            ValidBetween validBetween = new ValidBetween();
+
+            LocalDateTime fromDate = null;
+            LocalDateTime toDate = null;
+
+            for (LocalDateTime date : allDates) {
+                if (fromDate == null || date.isBefore(fromDate)) {
+                    fromDate = date;
+                }
+                if (toDate == null || date.isAfter(toDate)) {
+                    toDate = date;
+                }
+            }
+
+            validBetween.setFromDate(fromDate);
+            validBetween.setToDate(toDate);
+
+            marshaller.marshal(validBetween, writer);
+        }
 
         TypeOfFrameWriter.typeOfFrameWriter(writer, marshaller, typeNetex);
-
         writer.writeStartElement(MEMBERS);
-
         ServiceCalendarFrameFranceWriter.write(writer, context, exportableNetexData, marshaller);
-
         writer.writeEndElement();
     }
 }
