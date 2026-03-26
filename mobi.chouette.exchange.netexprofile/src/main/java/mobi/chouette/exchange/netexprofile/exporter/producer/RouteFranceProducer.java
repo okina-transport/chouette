e35e8c01f542fa7f5659c37eaaa2c087020a9d80
poswald <poswald@okina.fr>
2026-03-26 11:53:17 +0100

    fix: DATA-1729 FlexibleLineRef without :FlexibleLine:

--- mobi.chouette.exchange.netexprofile/src/main/java/mobi/chouette/exchange/netexprofile/exporter/producer/RouteFranceProducer.java
+++ mobi.chouette.exchange.netexprofile/src/main/java/mobi/chouette/exchange/netexprofile/exporter/producer/RouteFranceProducer.java
@@ -23,6 +23,10 @@ public class RouteFranceProducer extends NetexProducer implements NetexEntityPro
 
         netexRoute.setName(ConversionUtil.getMultiLingualString(neptuneRoute.getName()));
 
+        if(Boolean.TRUE.equals(neptuneRoute.getLine().getFlexibleService()) && neptuneRoute.getLine().getObjectId().contains(":FlexibleLine:")){
+            neptuneRoute.getLine().setObjectId(neptuneRoute.getLine().getObjectId().replace(":FlexibleLine:", ":Line:"));
+        }
+
         netexRoute.setLineRef(NetexProducerUtils.createLineIDFMRef(neptuneRoute.getLine(), netexFactory));
 
         netexRoute.setDirectionType(mapDirectionType(neptuneRoute.getDirection()));
