ca252f224c5053336c1c545456ec037164f98d27
afreche <alexandre.freche@lumiplan.com>
2026-04-21 09:58:51 +0200

    Revert "fix: DATA-1729 no longer carry the FlexibleLine by the line ID"
    This reverts commit 4fb36165c0bc68e129fcb1c4fc8ddb29b02ca0be.

--- mobi.chouette.exchange.netexprofile/src/main/java/mobi/chouette/exchange/netexprofile/exporter/producer/NetworkFranceProducer.java
+++ mobi.chouette.exchange.netexprofile/src/main/java/mobi/chouette/exchange/netexprofile/exporter/producer/NetworkFranceProducer.java
@@ -53,6 +53,9 @@ public class NetworkFranceProducer extends NetexProducer implements NetexEntityP
         LineRefs_RelStructure lineRefs_relStructure = netexFactory.createLineRefs_RelStructure();
         List<JAXBElement<? extends LineRefStructure>> jaxbElementsLineRefStructure = new ArrayList<>();
         for(Line line : neptuneNetwork.getLines()){
+            if (isFlexible(line) && !line.getObjectId().contains(":FlexibleLine:")) {
+                line.setObjectId(line.getObjectId().replace(":Line:", ":FlexibleLine:"));
+            }
             JAXBElement<? extends LineRefStructure> jaxbElementLineRefStructure = NetexProducerUtils.createLineIDFMRef(line, netexFactory);
             jaxbElementsLineRefStructure.add(jaxbElementLineRefStructure);
         }
