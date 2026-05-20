5b11ce2dbeca8396146ff1cd5e298ae78a9584bf
jmarzin1 <julien.marzin@lumiplan.com>
2026-05-20 14:56:07 +0200

    fix(DAO) : remove unused import

--- mobi.chouette.dao/src/main/java/mobi/chouette/dao/CleanUpDAOImpl.java
+++ mobi.chouette.dao/src/main/java/mobi/chouette/dao/CleanUpDAOImpl.java
@@ -5,13 +5,9 @@ import mobi.chouette.core.CoreExceptionCode;
 import org.joda.time.LocalDate;
 
 import javax.ejb.Stateless;
-import javax.ejb.TransactionAttribute;
-import javax.ejb.TransactionAttributeType;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.Query;
-import java.util.Date;
-import java.util.List;
 
 @Stateless
 public class CleanUpDAOImpl implements  CleanUpDAO{
